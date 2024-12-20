a770a1b13cd7e9df46784220c372cf41531e7b19
Carlo Antonio Venditti <c.a.venditti@pm.me>
2024-12-20 14:40:08 +0100

    Add startup script
    All the cool kids use the fish shell 😎

---
+++ start.fish
@@ -0,0 +1,30 @@
+#!/bin/fish
+
+# backend
+uv run fastapi dev &
+
+# frontend
+cd frontend/ttr-dashboard/
+CI=1 bunx expo start &
+
+# open web page
+sleep 2
+open http://localhost:8081 &
+
+echo
+echo "Started servers."
+echo
+
+# capture Ctrl+C with an event handler to stop both fastapi and expo servers
+function on_sigint --on-signal INT
+    echo "Caught Ctrl+C, stopping servers..."
+    kill %1
+    kill %2
+end
+
+wait
+
+# avoid fastapi messages messing up with the next prompt
+# (one could redirect output the right way but I'm lazy)
+sleep 1
+echo "Stopped."
